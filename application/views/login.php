10e053965899bc3afcc70cd39f1338ea542270eb
Horia <horia@geronimostudios.com>
2016-10-17 15:28:29 +0300

    first commit

---
+++ application/views/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login to UpWeGo</title>
+</head>
+<body>
+    <div>
+        <form>
+            User:<input type="text" placeholder="Insert your name here">
+            Password:<input type="password" placeholder="Insert your password here">
+            <select>
+                <option disabled selected>-- Select an option --</option>
+                <option>USER</option>
+                <option>ADMIN</option>
+            </select>
+        </form>
+    </div>
+</body>
+</html>
---
+++ application/views/second.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hstancu
+ * Date: 8/24/2016
+ * Time: 5:02 PM
+ */
+
+echo "Text scris";
+?>
