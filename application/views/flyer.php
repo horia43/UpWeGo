4e4c17dec9624a7669942694c484251909ab03fd
Stancu Horia Bogdan <stancu.horiabogdan@yahoo.com>
2017-06-14 03:53:43 +0300

    started the pay slip sample v3

--- application/views/flyer.php
+++ application/views/flyer.php
@@ -20,7 +20,7 @@
             top:150px;
             border:1px solid black;
             background-color:#D8D8D8;
-
+            padding-left:25px;
         }
 
         .h1 {
@@ -30,12 +30,14 @@
             padding-top: 50px;
             text-align: center;
             border-bottom:3px solid black;
+            font-weight: bolder;
         }
 
         .h2 {
             font-size: 30px;
             padding-top: 5px;
             text-align: center;
+            font-weight: bolder;
         }
         #logo{
             position:relative;
@@ -43,170 +45,70 @@
             right:70px;
             width:179px;
             height:200px;
-            background: url(<?php echo base_url()?>/upload/UpWeGo_Logo.png);
+            background: url(<?php echo base_url();?>/upload/UpWeGo_Logo.png);
             float:right;
             clear:left;
         }
-        p:first-child {
-            font-size: 28px;
-            color: #376a1f;
-            margin-bottom: 1px;
-            margin-top: 45px;
-            padding-top: 1px;
-            padding-bottom: 1px;
+        .details{
+            padding-top: 15px;
         }
-
-        div p {
-            font-size: 28px;
-            color: #2faca9;
-
+        .details p {
+            font-size: 13px;
             margin-top: 1px;
             margin-bottom: 1px;
             padding-top: 1px;
             padding-bottom: 1px;
-
+        }
+        .details p span{
+            font-weight: bold;
+        }
+        .details2 p{
+            font-weight: bold;
+            margin-top:10px;
+            margin-bottom:0px;
+        }
+        table{
+            width:80%;
+            margin:0 auto;
+        }
+        table, th, td {
+            border: 1px solid black;
         }
 
-        p:last-child {
-            margin-bottom: 45px;
-        }
 
-        .margintop {
-            margin-top: 70px;
-        }
-
-        .common {
-            border-bottom: 2px solid #fffad3;
-            margin-left: 45px;
-            margin-right: 150px;
-        }
-
-        .section1 {
-            font-size: 20px;
-            color: #bcb8b0;
-            margin-top: 35px;
-            margin-bottom: 35px;
-        }
-
-        .section2 {
-            margin-bottom: 45px;
-        }
-
-        .section2 img {
-            margin-right: 20px;
-            margin-top: 20px;
-        }
-
-        .pieces {
-            display: inline-block;
-        }
-
-        .pieces p {
-            font-size: 28px;
-            color: #23bc8d;
-        }
-
-        #pieces1 {
-            margin-right: 30px;
-            margin-top: 15px;
-            margin-bottom: 45px;
-
-        }
-
-        #pieces2 {
-            margin-top: 15px;
-            margin-bottom: 45px;
-        }
-
-        .common2 {
-            margin-left: 45px;
-            margin-right: 150px;
-        }
     </style>
 
 </head>
 <body>
 <div id="container-parent">
     <div id="container">
-        <div style="width:100%; height:380px; font-weight: bolder; overflow: hidden; ">
+        <div style="width:100%; height:auto; overflow: hidden; ">
             <div class="h1">UpWeGo</div><hr style="width:160px; height:2px; background-color:#3c3c3c;"><hr style="width:120px; height:2px;background-color:#5f5f5f;">
             <div class="h2" style="clear:both;">Pay Slip</div>
-            <div id="logo">
+            <div id="logo"></div>
+            <div class="details">
+                <p><span>Adress:</span> Calea Septembrie 13 , nr. 113	, Bucuresti	sector 5</p>
+                <p><span>Phone:</span> 021 224 75 21</p>
+                <p><span>Fax:</span> 021 224 75 20</p>
+                <p><a href="www.upwego.com">www.upwego.com</a></p>
             </div>
         </div>
-        <div class="common">
-            <p>Detalii 1</p>
-            <p>Nume Prenume</p>
-            <p>Zi de nastere</p>
-            <p>Email</p>
+        <div class="details2">
+            <p>Name if Employee:</p>
+            <p>Year and Month:</p>
         </div>
-        <div class="common">
-            <p>Nume Prenume</p>
-            <p>Zi nastere</p>
-            <p>Email</p>
-        </div>
-        <div class="common">
-            <p>Descriere</p>
-            <p>Adresa</p>
-            <p>Salariu</p>
-            <p>Pozitie ocupata</p>
-            <p>Program de lucru</p>
-            <p>xdwasd</p>
-            <p>hsdfbgse</p>
-            <p>fesfzfeazf</p>
-            <p>sefvdxservaer</p>
-            <p>gfdrveg</p>
-        </div>
-        <div class="common">
-            <p>Zodie</p>
-            <p class="margintop">Astrologie</p>
-            <section class="section1">[RoomDescription1] ------- Lorem Ipsum is simply dummy text of the printing and
-                typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when
-                an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived
-                not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-            </section>
-            <div class="section2">
-                <img src="http://placehold.it/350//000000?text=[Zodiac-1]">
-                <img src="http://placehold.it/350//000000?text=[Zodiac-2]">
-                <img src="http://placehold.it/350//000000?text=[Zodiac-3]">
-            </div>
+        <table>
+            <th colspan="2">Scale of payment</th>
+            <tr>
+                <td>s</td>
+                <td>sadwa</td>
+            </tr>
+            <tr>
+                <td>bs</td>
+            </tr>
+        </table>
 
-            <p class="margintop">Compatibilitati</p>
-            <section class="section1">[RoomDescription2] ------- Lorem Ipsum is simply dummy text of the printing and
-                typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when
-                an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived
-                not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-            </section>
-            <div class="section2">
-                <img src="http://placehold.it/370//000000?text=[Exemplu-1]">
-                <img src="http://placehold.it/370//000000?text=[Exemplu-2]">
-                <img src="http://placehold.it/370//000000?text=[Exemplu-3]">
-                <img src="http://placehold.it/370//000000?text=[Exemplu-4]">
-                <img src="http://placehold.it/370//000000?text=[Exemplu-5]">
-            </div>
-        </div>
-        <div class="common">
-            <p>Informatii generale</p>
-            <div class="pieces">
-                <p>Info 1</p>
-                <img id="pieces1" src="http://placehold.it/600x350//000000?text=[Verde de Paris]">
-            </div>
-            <div class="pieces">
-                <p>Info 2</p>
-                <img id="pieces2" src="http://placehold.it/600x350//000000?text=[Mos Craciun]">
-            </div>
-        </div>
-        <div class="common2">
-            <p>Arta digitala</p>
-            <div class="pieces">
-                <p>Creion</p>
-                <img id="pieces1" src="http://placehold.it/600x350//000000?text=[Dragon-creion]">
-            </div>
-            <div class="pieces">
-                <p>Pastel</p>
-                <img id="pieces2" src="http://placehold.it/600x350//000000?text=[Dragon-pastel]">
-            </div>
-        </div>
+
     </div>
 </div>
 
